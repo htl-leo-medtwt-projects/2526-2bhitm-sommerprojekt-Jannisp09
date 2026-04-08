10cbacf7075d92f4599db1a4374c6090c321cd86
Jannis Pal <j.pal@students.htl-leonding.ac.at>
2026-04-08 10:27:16 +0200

    Add key event listeners for arrow keys
    - Implemented keyListenerDown and keyListenerUp functions to track the state of arrow keys (left, right, up, down).
    - Introduced KEY_EVENTS object to store the current state of each arrow key.

---
+++ project/script_keyevents.js
@@ -0,0 +1,42 @@
+
+/***********************************
+ * EVENT EVENTS
+ ***********************************/
+let KEY_EVENTS = {
+    leftArrow: false,
+    rightArrow: false,
+    upArrow: false,
+    downArrow: false
+}
+document.onkeydown = keyListenerDown;
+document.onkeyup = keyListenerUp;
+
+function keyListenerDown(e) {
+    if (e.key === "ArrowLeft") { // Left arrow
+        KEY_EVENTS.leftArrow = true;
+    }
+    if (e.key === "ArrowUp") { // Up arrow
+        KEY_EVENTS.upArrow = true;
+    }
+    if (e.key === "ArrowRight") { // Right arrow
+        KEY_EVENTS.rightArrow = true;
+    }
+    if (e.key === "ArrowDown") { // Down arrow
+        KEY_EVENTS.downArrow = true;
+    }
+}
+function keyListenerUp(e) {
+    if (e.key === "ArrowLeft") { // Left arrow
+        KEY_EVENTS.leftArrow = false;
+    }
+    if (e.key === "ArrowUp") { // Up arrow
+        KEY_EVENTS.upArrow = false;
+    }
+    if (e.key === "ArrowRight") { // Right arrow
+        KEY_EVENTS.rightArrow = false;
+    }
+    if (e.key === "ArrowDown") { // Down arrow
+        KEY_EVENTS.downArrow = false;
+    }
+}
+
