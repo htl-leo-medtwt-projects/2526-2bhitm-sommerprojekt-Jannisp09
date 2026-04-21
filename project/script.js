0594988c113c3f8d952b7416367476502a257dcd
Jannis Pal <j.pal@students.htl-leonding.ac.at>
2026-04-21 18:51:17 +0200

    Add inventory overlay with UI elements and functionality

--- project/script.js
+++ project/script.js
@@ -6,12 +6,14 @@ let settingScreen = document.getElementById("settingsScreen");
 let musicIcon = document.getElementById("musicIcon");
 let musicText = document.getElementById("musicText");
 let setupScreen = document.getElementById("setupScreen");
+let inventoryOverlay = document.getElementById("inventoryOverlay");
 let level1 = document.getElementById("levelOne");
 
 startScreen.style.display = "grid";
 settingScreen.style.display = "none";
 setupScreen.style.display = "none";
 level1.style.display = "none";
+inventoryOverlay.style.display = "none";
 
 
 function startDisplay() {
@@ -19,6 +21,7 @@ function startDisplay() {
     settingScreen.style.display = "none";
     setupScreen.style.display = "none";
     level1.style.display = "none";
+    inventoryOverlay.style.display = "none";
 }
 
 function settings() {
@@ -26,6 +29,7 @@ function settings() {
     settingScreen.style.display = "grid";
     setupScreen.style.display = "none";
     level1.style.display ="none";
+    inventoryOverlay.style.display = "none";
 }
 
 function backToStart() {
@@ -33,6 +37,7 @@ function backToStart() {
     settingScreen.style.display = "none";
     setupScreen.style.display = "none";
     level1.style.display ="none";
+    inventoryOverlay.style.display = "none";
 }
 
 function startMusic() {
@@ -44,7 +49,17 @@ function startSetup() {
     startScreen.style.display = "none";
     settingScreen.style.display = "none";
     setupScreen.style.display = "grid";
-    level1.style.display ="none";
+    level1.style.display = "none";
+    inventoryOverlay.style.display = "none";
+}
+
+function backToGame() {
+    inventoryOverlay.style.display = "none";
+}
+
+function openInventory() {
+    inventoryOverlay.style.display = "grid";
+    console.log("Inventory geöffnet");
 }
 
 let playerName = "";
